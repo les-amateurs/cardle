e3cd9146777f3a7fce06431316fc6e7128145c76
unvariant <unvariant.winter@gmail.com>
2024-10-09 11:21:47 -0700

    add stuff

--- split.py
+++ split.py
@@ -3,10 +3,33 @@ from PIL import Image
 IDS = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]
 SUITS = ["hearts", "clubs", "diamonds", "spades"]
 
+border = Image.open("src/assets/backs/back-0-0.png")
+borderdata = border.load()
+width, height = border.size
+for y in range(height):
+    for x in range(width):
+        if borderdata[x, y][3] != 0:
+            borderdata[x, y] = (255, 255, 255, 255)
+
 for i in range(4):
     for j in range(13):
         box = (j * 71, i * 95, (j + 1) * 71, (i + 1) * 95)
         sheet = Image.open("balatro.png")
-        print(box)
         new = sheet.crop(box)
+
+        pixdata = new.load()
+        width, height = new.size
+        for y in range(height):
+            for x in range(width):
+                if pixdata[x, y][3] == 0:
+                    pixdata[x, y] = borderdata[x, y]
         new.save(f"src/assets/cards/{IDS[j]}-of-{SUITS[i]}.png")
+
+sheet = Image.open("backs.png")
+dw = sheet.width / 7
+dh = sheet.height / 5
+for i in range(5):
+    for j in range(7):
+        box = (j * dw, i * dh, (j + 1) * dw, (i + 1) * dh)
+        new = sheet.crop(box)
+        new.save(f"src/assets/backs/back-{i}-{j}.png")
--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,216 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import balatroKing from "./assets/cards/two-of-diamonds.png";
+import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
-function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [name, setName] = useState("");
+const IDS = [
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "jack",
+    "queen",
+    "king",
+    "ace",
+];
+const SUITS = ["hearts", "clubs", "diamonds", "spades"];
 
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        setGreetMsg(await invoke("greet", { name }));
+interface Card {
+    state: string;
+    n: number;
+}
+
+interface Game {
+    board: Card[][];
+    greens: number[];
+    yellows: Map<number, number[]>;
+    grays: number[];
+}
+
+function numberToCard(card: Card) {
+    const suit = 0;
+    if (card.state === "U") {
+        return "./src/assets/backs/back-0-0.png";
     }
+    return `./src/assets/cards/${IDS[card.n]}-of-${SUITS[suit]}.png`;
+}
 
-    const cards = [];
-    for (let i = 0; i < 4; i++) {
-        cards.push(
-            <img
-                src={balatroKing}
-                style={{
-                    width: "100%",
-                    objectFit: "contain",
-                    imageRendering: "-moz-crisp-edges",
-                }}
-            ></img>
+function renderCard(card: Card, style: React.CSSProperties) {
+    return (
+        <img
+            src={numberToCard(card)}
+            style={{
+                objectFit: "contain",
+                imageRendering: "-moz-crisp-edges",
+                borderColor: "gray",
+                borderWidth: "1px",
+                borderStyle: "solid",
+                height: "100%",
+                ...style,
+            }}
+        ></img>
+    );
+}
+
+function renderColumn(cards: Card[], currentRow: number) {
+    const r = [];
+    for (let i = 0; i < cards.length; i++) {
+        if (i == currentRow) {
+            r.push(renderCard(cards[i], { backgroundColor: "gray" }));
+        } else {
+            r.push(renderCard(cards[i], { backgroundColor: "inherit" }));
+        }
+    }
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+            }}
+        >
+            {r}
+        </div>
+    );
+}
+
+function renderBoard(game: Game, currentGuess: number, currentRow: number) {
+    const r = [];
+    for (let i = 0; i < game.board.length; i++) {
+        const col = game.board[i];
+        if (i == currentGuess) {
+            r.push(renderColumn(col, currentRow));
+        } else {
+            r.push(renderColumn(col, -1));
+        }
+    }
+    return r.reverse();
+}
+
+function renderOptions(game: Game) {
+    const options = [];
+    for (let i = 0; i < IDS.length; i++) {
+        const style: React.CSSProperties = {};
+        if (game.greens.indexOf(i) != -1 || game.grays.indexOf(i) != -1 || game.yellows.has(i)) {
+            style.filter = "brightness(30%)";
+        }
+        const card = renderCard(
+            {
+                state: "S",
+                n: i,
+            },
+            style,
         );
+        options.push(card);
     }
 
     return (
         <div
             style={{
-                borderColor: "gray",
-                borderWidth: "2px",
-                borderStyle: "solid",
+                display: "flex",
+                flexDirection: "row",
+                height: "20vh"
+            }}
+        >
+            {options}
+        </div>
+    );
+}
+
+function updateOptions(
+    board: object[][],
+    selectedIndex: number,
+    setOptions: any
+) {
+    const options = [];
+}
+
+function randomAnswer() {
+    const answer = [];
+    for (let i = 0; i < 4; i++) {
+        answer.push({
+            state: "H",
+            n: Math.floor(Math.random() * 13),
+        });
+    }
+    return answer;
+}
+
+function App() {
+    const [answer, setAnswer] = useState(randomAnswer());
+    const [game, setGame] = useState({
+        board: Array(10)
+            .fill(0)
+            .map((u) =>
+                Array(4).fill({
+                    state: "U",
+                    number: undefined,
+                })
+            ),
+        greens: [0],
+        yellows: new Map(),
+        grays: [],
+    });
+    const [currentGuess, setCurrentGuess] = useState(0);
+    const [currentRow, setCurrentRow] = useState(0);
+    const [lastPressedKey, setLastPressedKey] = useState(null);
+    const [currentOptions, setCurrentOptions] = useState({});
+
+    const handleKeyPress = (event: KeyboardEvent) => {
+        let row = currentRow;
+        switch (event.key) {
+            case "ArrowDown":
+                row = currentRow + 1;
+                break;
+            case "ArrowUp":
+                row = currentRow - 1;
+                break;
+        }
+        if (row < 0) {
+            row += 4;
+        }
+        setCurrentRow(row % 4);
+    };
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyPress);
+
+        return () => window.removeEventListener("keydown", handleKeyPress);
+    }, [currentRow]);
+
+    return (
+        <div
+            style={{
                 display: "flex",
                 flexDirection: "column",
+                height: "100vh",
+                width: "100%",
+                margin: "auto",
+                justifyContent: "center",
+                alignItems: "center",
             }}
         >
             <div
                 style={{
+                    borderColor: "gray",
+                    borderWidth: "1px",
+                    borderStyle: "solid",
                     display: "flex",
                     flexDirection: "row",
+                    height: "70vh",
+                    width: "100%",
                 }}
             >
-                {cards}
+                {renderColumn(answer, -1)}
+                {renderBoard(game, currentGuess, currentRow)}
             </div>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                }}
-            >
-                {cards}
-            </div>
+
+            {renderOptions(game)}
         </div>
     );
-
-    // return (
-    //   <div className="container">
-    //     <h1>Welcome to Tauri!</h1>
-
-    //     <div className="row">
-    //       <a href="https://vitejs.dev" target="_blank">
-    //         <img src={balatroKing} className="logo vite" alt="Vite logo" />
-    //       </a>
-    //       <a href="https://tauri.app" target="_blank">
-    //         <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-    //       </a>
-    //       <a href="https://reactjs.org" target="_blank">
-    //         <img src={reactLogo} className="logo react" alt="React logo" />
-    //       </a>
-    //     </div>
-
-    //     <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-    //     <form
-    //       className="row"
-    //       onSubmit={(e) => {
-    //         e.preventDefault();
-    //         greet();
-    //       }}
-    //     >
-    //       <input
-    //         id="greet-input"
-    //         onChange={(e) => setName(e.currentTarget.value)}
-    //         placeholder="Enter a name..."
-    //       />
-    //       <button type="submit">Greet</button>
-    //     </form>
-
-    //     <p>{greetMsg}</p>
-    //   </div>
-    // );
 }
 
 export default App;
